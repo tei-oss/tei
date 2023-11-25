3db5faa22e820c5444b5e3ad8ea09edbeb90b18c
koheatel <koheatel@gmail.com>
2023-11-25 23:28:41 +0600

    feat: Implement tag indexing in meilisearch

--- server/sandbox/src/main.rs
+++ server/sandbox/src/main.rs
@@ -1,9 +1,11 @@
 use deadpool_postgres::{tokio_postgres::NoTls, Config, Runtime};
+use meilisearch_sdk::Client;
 use tei_core::{
     auxiliary::Audit,
     tag::{Tag, TagId},
 };
-use tei_data::tags::TagsDb;
+
+use tei_filter::tags::TagIndex;
 use tracing::{subscriber::set_global_default, Level};
 
 #[tokio::main]
@@ -23,7 +25,7 @@ async fn main() {
     config.application_name = Some("sandbox".to_owned());
 
     let pool = config.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
-    let dbc = pool
+    let _dbc = pool
         .get()
         .await
         .expect("Could not get DB connection from pool");
@@ -32,14 +34,22 @@ async fn main() {
     let user_id = 1.into();
 
     let tag = Tag {
-        id: TagId::new(group_id, 1),
-        label: "ai".into(),
-        description: Some("6 finger nightmares".into()),
+        id: TagId::new(group_id, 3),
+        label: "rust".into(),
+        description: Some("fearless concurrency".into()),
         color: None,
         icon: None,
         group_id,
         audit: Audit::created(&user_id),
     };
 
-    TagsDb::insert(&dbc, &tag).await.unwrap();
+    // TagsDb::insert(&dbc, &tag).await.unwrap();
+
+    let api_key: Option<String> = None;
+    let meili = Client::new("http://localhost:17700", api_key);
+
+    let tag_index = TagIndex::new(&meili);
+
+    tag_index.configure().await.unwrap();
+    tag_index.add([&tag]).await.unwrap();
 }
