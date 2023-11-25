bd15fd169e7390a20067422a075ec4eabda5ffa0
koheatel <koheatel@gmail.com>
2023-11-25 20:34:14 +0600

    Add wal listener

--- server/sandbox/src/main.rs
+++ server/sandbox/src/main.rs
@@ -1,4 +1,9 @@
 use deadpool_postgres::{tokio_postgres::NoTls, Config, Runtime};
+use tei_core::{
+    auxiliary::Audit,
+    tag::{Tag, TagId},
+};
+use tei_data::tags::TagsDb;
 
 #[tokio::main]
 async fn main() {
@@ -7,9 +12,23 @@ async fn main() {
     config.user = Some("localc".to_owned());
     config.password = Some("localc".to_owned());
     config.port = Some(15432);
+    config.application_name = Some("sandbox".to_owned());
 
-    let _pool = config.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
-    let mut _conn = _pool.get().await.unwrap();
+    let pool = config.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+    let mut dbc = pool.get().await.unwrap();
 
-    todo!()
+    let group_id = 1.into();
+    let user_id = 1.into();
+
+    let tag = Tag {
+        id: TagId::new(group_id, 1),
+        label: "ai".into(),
+        description: Some("6 finger nightmares".into()),
+        color: None,
+        icon: None,
+        group_id,
+        audit: Audit::created(&user_id),
+    };
+
+    TagsDb::insert(&dbc, &tag).await.unwrap();
 }
