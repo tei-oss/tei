48ccf447ee9b906612daf1c92ffacb7d18084f9d
koheatel <koheatel@gmail.com>
2023-11-25 19:31:37 +0600

    Wait for postgres healthcheck in  dev-up.sh

--- dev-up.sh
+++ dev-up.sh
@@ -4,9 +4,9 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo "${LGREEN}• Booting up docker containers${NC}"
-docker compose up -d
+docker compose up -d --wait || exit 1
 
 echo "\n${LGREEN}• Applying migrations${NC}"
-refinery migrate -c ./server/tei-data/refinery.toml -p ./server/tei-data/migrations
+refinery migrate -c ./server/tei-data/refinery.toml -p ./server/tei-data/migrations || exit 1
 
 echo "\n✔ ${GREEN}Environment successfully created${NC}"
