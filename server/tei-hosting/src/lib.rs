faa9fed347c340e3efdbfb918b9036f85744c70b
koheatel <koheatel@gmail.com>
2023-11-30 22:31:15 +0600

    feat: add env-based logging conig

--- server/tei-hosting/src/lib.rs
+++ server/tei-hosting/src/lib.rs
@@ -1,13 +1,14 @@
 use color_eyre::eyre::Result;
-use tracing::{subscriber::set_global_default, Level};
+use tracing::subscriber::set_global_default;
+use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter};
 
 pub fn install() -> Result<()> {
     color_eyre::install()?;
     dotenvy::dotenv().expect(".env file not found");
 
-    let collector = tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
-        .finish();
+    let collector = tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env());
 
     set_global_default(collector)?;
 
