7078c5d0c68b8481c5edafb9eb4d5b5e12db7934
Konstatin <koheatel@gmail.com>
2023-11-30 22:48:33 +0600

    feat: add empty frontend project (#9)
    * Init frontend svelte app
    
    * Add frontend to the start-all.sh script

--- start-all.sh
+++ start-all.sh
@@ -1,13 +1,18 @@
 ./dev-up.sh
 
-cd server
-
 trap stopall INT
+
+cd server
 
 cargo watch -x "run --bin tei-api" &
 pid1=$!
 cargo watch -x "run --bin tei-indexd" &
 pid2=$!
+
+cd ../client/tei-web
+npm i
+npm run dev &
+pid3=$!
 
 stopall() {
 
@@ -15,6 +20,7 @@ stopall() {
 
   kill -2 $pid1
   kill -2 $pid2
+  kill -3 $pid3
 
   pkill tei-api
   pkill tei-index
